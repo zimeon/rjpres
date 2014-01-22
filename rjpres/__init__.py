2118bbedee33a9ca62a374f5deefd9f1030daf0b
Simeon Warner <simeon.warner@cornell.edu>
2014-01-22 14:25:15 -0500

    Module structure and binary

---
+++ rjpres/__init__.py
@@ -0,0 +1,3 @@
+from _version import __version__
+
+from http_server import RjpresHTTPRequestHandler
---
+++ rjpres/_version.py
@@ -0,0 +1,5 @@
+# This is the one place the version number for rjpres is stored
+#
+# Format: x.y
+__version__ = '0.1'
+
